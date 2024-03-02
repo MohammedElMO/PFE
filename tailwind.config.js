eb414121b2d4fc4b23a82aa7be5809882bc52d56
MohammedElMO <elamranimohammed104@gmail.com>
2024-03-02 17:30:07 +0100

    configuring tailwind and adding utility class from css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "src/components/**/*.html"],
+
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily:{
+        "pop":["Poppins","sans-serif"]
+      }
+
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
 };
