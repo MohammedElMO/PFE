e063eec23e946cbc6fc8dbb56201f48a5340af3b
MohammedElMO <elamranimohammed104@gmail.com>
2024-03-03 23:31:54 +0100

    working on a sidebar UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "src/components/**/*.html"],
+  darkMode: "selector",
 
   theme: {
-    
     extend: {
-      fontFamily:{
-        "pop":["Poppins","sans-serif"]
-      }
-
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"],
+      },
+      background: {
+        side: `linear-gradient(
+          303deg,
+          rgba(120, 26, 235, 1) 20%,
+          rgba(32, 32, 186, 0.8631827731092436) 65%
+        )`,
+      },
     },
   },
   plugins: ["prettier-plugin-tailwindcss"],
-};
+}
