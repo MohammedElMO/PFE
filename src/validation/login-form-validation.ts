e9c0bd6dc123ce1f92cddfcbaa6fcc16e670bdfb
MohammedElMO <elamranimohammed104@gmail.com>
2024-03-07 20:25:26 +0100

    applaying focus effect on the first input in login/page

--- src/validation/login-form-validation.ts
+++ src/validation/login-form-validation.ts
@@ -1,83 +1,87 @@
-import { SafeParseReturnType, ZodFormattedError } from "zod"
+import { SafeParseReturnType } from "zod"
 import { LoginT, loginSchema } from "../schema/login-schema.zod"
 import "../styles/utils.css"
-import { loginSuccessToater } from "../utils/toaster-notifier"
 
 const loginForm = <HTMLFormElement>document.querySelector("form")
 const password = document.querySelector("#password") as HTMLInputElement
 const username = document.querySelector("#username") as HTMLInputElement
+const cancelBtn = document.querySelector(".cancel") as HTMLButtonElement
 const loginBtn = document.querySelector(".log-in") as HTMLButtonElement
+const remembreMeFlag = document.querySelector(
+    "#remember-me",
+) as HTMLInputElement
 
-loginForm.addEventListener("submit", loginHandler)
-loginBtn.addEventListener("click", () => {
-  loginSuccessToater()
-})
-password.addEventListener("input", ValidationHandler)
-username.addEventListener("input", ValidationHandler)
 const passErrMessage = document.createElement("p")
 const userErrMessage = document.createElement("p")
 loginBtn.disabled = true
-let formater: ZodFormattedError<LoginT, string>
-let form
+let form = null
 let isValidLogin: SafeParseReturnType<LoginT, LoginT>
 
+addEventListener("DOMContentLoaded", () =>
+    username.focus({ preventScroll: true }),
+)
+cancelBtn.addEventListener("click", resetForm)
+loginForm.addEventListener("submit", loginHandler)
+password.addEventListener("input", ValidationHandler)
+username.addEventListener("input", ValidationHandler)
+
 function loginHandler(e: SubmitEvent) {
-  e.preventDefault()
-  // console.log(formater?.password?._errors)
-  // console.log(formater?.username?._errors)
-  // if (isValidLogin.success) {
-  //   alert("login success")
-  // }
-  // let data = Object.fromEntries(new FormData(loginForm))
-  // console.log(data)
+    e.preventDefault()
+    if (isValidLogin.success) alert("booom")
 }
 
-function ValidationHandler(e: Event) {
-  e.preventDefault()
-  form = new FormData(loginForm)
-  const { username: inUserName, password: inPassword } =
-    Object.fromEntries(form)
+function ValidationHandler() {
+    form = new FormData(loginForm)
+    const { username: inUserName, password: inPassword } =
+        Object.fromEntries(form)
+    // console.log(inUserName, inPassword)
+    isValidLogin = loginSchema.safeParse({
+        username: inUserName,
+        password: inPassword,
+    })
 
-  isValidLogin = loginSchema.safeParse({
-    username: inUserName,
-    password: inPassword,
-  })
-  console.log(form.get("username"))
+    if (!isValidLogin.success) {
+        loginBtn.disabled = true
+        let formater = isValidLogin.error.formErrors.fieldErrors
+        console.log(formater)
 
-  if (!isValidLogin.success) {
-    loginBtn.disabled = true
-    formater = isValidLogin.error.format()
-    console.log(formater)
-    if (formater.password?._errors) {
-      injectErr(
-        formater.password._errors,
-        "password-err",
-        passErrMessage,
-        password,
-      )
+        if (formater?.password) {
+            injectErr(
+                formater.password,
+                "password-err",
+                passErrMessage,
+                password,
+            )
+        } else {
+            passErrMessage.textContent = ""
+        }
+        if (formater?.username) {
+            injectErr(formater.username, "user-err", userErrMessage, username)
+        } else {
+            userErrMessage.textContent = ""
+        }
+    } else {
+        loginBtn.disabled = false
     }
-    if (formater.username?._errors) {
-      injectErr(
-        formater.username?._errors,
-        "user-err",
-        userErrMessage,
-        username,
-      )
-    }
-  } else {
-    passErrMessage.textContent = ""
-    userErrMessage.textContent = ""
-    loginBtn.disabled = false
-  }
 }
 
 function injectErr(
-  errors: string[],
-  errClass: string,
-  errDisplayer: HTMLParagraphElement,
-  input: HTMLInputElement,
+    errors: string[],
+    errClass: string,
+    errDisplayer: HTMLParagraphElement,
+    input: HTMLInputElement,
 ) {
-  errDisplayer.classList.add(errClass)
-  errDisplayer.textContent = errors.join("\n")
-  input.parentElement?.after(errDisplayer)
+    errDisplayer.classList.add(errClass)
+    errDisplayer.textContent = errors.join("\n")
+    input.parentElement?.after(errDisplayer)
+}
+
+function resetForm() {
+    passErrMessage.textContent = ""
+    userErrMessage.textContent = ""
+    loginBtn.disabled = true
+    form = null
+    username.value = ""
+    password.value = ""
+    remembreMeFlag.checked = false
 }
